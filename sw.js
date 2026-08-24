fb614172c81d48a122db6246f724c16bef5dd186
jesuscruz1984 <84680308+jesuscruz1984@users.noreply.github.com>
2026-08-24 08:41:59 -0400

    Bump EARA cache for smart web voice upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='eara-online-v12';
+const C='eara-online-v13';
 const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','app.js','handsfree.js','robot-bg.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
