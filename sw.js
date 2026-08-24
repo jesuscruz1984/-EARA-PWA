4537c56a05693ec154764569c924d721607c0de1
jesuscruz1984 <84680308+jesuscruz1984@users.noreply.github.com>
2026-08-24 11:49:18 -0400

    Bump EARA cache to v18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='eara-online-v16';
+const C='eara-online-v18';
 const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','app.js','handsfree.js','robot-bg.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
