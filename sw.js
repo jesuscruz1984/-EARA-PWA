f228e273ac86a1f7d4305a3fd485ae860f9411ed
jesuscruz1984 <84680308+jesuscruz1984@users.noreply.github.com>
2026-08-24 21:50:44 -0400

    Bump EARA cache to v30 with picture fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='eara-online-v29';
-const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','app.js','picture.js','noise.js','handsfree.js','robot-bg.svg'];
+const C='eara-online-v30';
+const A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','app.js','picture.js','picturefix.js','noise.js','handsfree.js','robot-bg.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>{const req=e.request;if(req.mode==='navigate'){e.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match('index.html')));return}e.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match(req)))});
